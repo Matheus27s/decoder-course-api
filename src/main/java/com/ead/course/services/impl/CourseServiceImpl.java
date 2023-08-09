f144c73b6ad3c284fc89eb6513df28d4e1dbc718
Matheus Sousa <mssantos19@stefanini.com>
2023-08-09 11:07:02 -0300

    4. Deleção de Relacionamento Cascade.ALL ou Método de Deleção Customizado

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,51 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     @Autowired
-    CourseRepository repository;
+    CourseRepository courseRepository;
+
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Override
+    @Transactional
+    public void delete(CourseModel courseModel) {
+        //Todo Retorna os modules de um determinado course
+        List<ModuleModel> moduleModelList = moduleRepository
+                .findAllModulesIntoCourse(courseModel.getCourseId());
+
+        if(!moduleModelList.isEmpty()) {
+            for(ModuleModel moduleModel : moduleModelList) {
+
+                //Todo Retorna os lessons de um determinado module
+                List<LessonModel> lessonModelList = lessonRepository
+                        .findAllLessonsIntoModule(moduleModel.getModuleId());
+                if(!lessonModelList.isEmpty()) {
+
+                    //Todo Deleta todas as lessons de um determinado module
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
+    }
 }
